f2dbb587f73a234575ef7ffe578e8bb14a3b6c64
Davi Roberto <daviroberto11@hotmail.com>
2022-03-06 14:18:36 -0300

    login and dashboard

---
+++ src/components/Form/Input.tsx
@@ -0,0 +1,30 @@
+import {
+  FormControl,
+  FormLabel,
+  Input as ChakraInput,
+  InputProps as ChakaraInputProps,
+} from '@chakra-ui/react';
+
+interface InputProps extends ChakaraInputProps {
+  name: string;
+  label?: string;
+}
+
+export function Input({ name, label, ...rest }: InputProps) {
+  return (
+    <FormControl>
+      {!!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
+
+      <ChakraInput
+        name={name}
+        id={name}
+        focusBorderColor="pink.500"
+        bgColor="gray.900"
+        variant="filled"
+        size="lg"
+        _hover={{ bgColor: 'gray.900' }}
+        {...rest}
+      />
+    </FormControl>
+  );
+}
