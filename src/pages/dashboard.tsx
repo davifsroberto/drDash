177c9a1979b905f45a7c2a027e6202727fac4562
Davi Roberto <daviroberto11@hotmail.com>
2022-03-06 17:10:28 -0300

    login and dashboard - END

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,15 +1,91 @@
-import { Flex } from '@chakra-ui/react';
+import dynamic from 'next/dynamic';
 
+import { Box, Flex, SimpleGrid, Text } from '@chakra-ui/react';
+import { ApexOptions } from 'apexcharts';
+
+import { theme } from '../styles/theme';
 import { Header } from '../components/Header';
 import { Sidebar } from '../components/Sidebar';
+
+const Chart = dynamic(() => import('react-apexcharts'), {
+  ssr: false,
+});
+
+const options: ApexOptions = {
+  chart: {
+    toolbar: { show: false },
+    zoom: {
+      enabled: false,
+    },
+    foreColor: theme.colors.gray[500],
+  },
+  grid: { show: false },
+  dataLabels: { enabled: false },
+  tooltip: { enabled: false },
+  xaxis: {
+    type: 'datetime',
+    axisBorder: { color: theme.colors.gray[600] },
+    axisTicks: { color: theme.colors.gray[600] },
+    categories: [
+      '2022-03-19T00:00:00.000Z',
+      '2022-03-20T00:00:00.000Z',
+      '2022-03-21T00:00:00.000Z',
+      '2022-03-22T00:00:00.000Z',
+      '2022-03-23T00:00:00.000Z',
+      '2022-03-24T00:00:00.000Z',
+      '2022-03-25T00:00:00.000Z',
+    ],
+  },
+  fill: {
+    opacity: 0.3,
+    type: 'gradient',
+    gradient: {
+      shade: 'dark',
+      opacityFrom: 0.7,
+      opacityTo: 0.3,
+    },
+  },
+};
+
+const series = [{ name: 'series-1', data: [31, 40, 28, 51, 42, 109, 100] }];
 
 export default function Dashboard() {
   return (
     <Flex direction="column" h="100vh">
       <Header />
 
-      <Flex w="100%" my="6" mx="auto" px="6" maxWidth={1480}>
+      <Flex w="100%" mx="auto" my="6" px="6" maxWidth={1480}>
         <Sidebar />
+
+        <SimpleGrid
+          flex="1"
+          gap="4"
+          minChildWidth="320px"
+          alignContent="flex-start"
+        >
+          <Box p="8" bg="gray.800" borderRadius={8}>
+            <Text fontSize="lg" mb="4">
+              Week Subscriptions
+            </Text>
+
+            <Chart type="area" height={160} options={options} series={series} />
+          </Box>
+        </SimpleGrid>
+
+        <SimpleGrid
+          flex="1"
+          gap="4"
+          minChildWidth="320px"
+          alignContent="flex-start"
+        >
+          <Box p="8" bg="gray.800" borderRadius={8}>
+            <Text fontSize="lg" mb="4">
+              Opening
+            </Text>
+
+            <Chart type="area" height={160} options={options} series={series} />
+          </Box>
+        </SimpleGrid>
       </Flex>
     </Flex>
   );
