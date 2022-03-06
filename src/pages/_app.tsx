b351e8eaa45d5c1bc6f625087e7d2759027f9ae5
Davi Roberto <daviroberto11@hotmail.com>
2022-03-05 22:33:58 -0300

    config init - END

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,8 @@
-import { AppProps } from "next/app";
+import { AppProps } from 'next/app';
 
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider } from '@chakra-ui/react';
 
-import { theme } from "../styles/theme";
+import { theme } from '../styles/theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
